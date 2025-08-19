392975442f438c1d27f0b9467706910e5c4bba3c
wonderlust <pandeyshashikant513@gmail.com>
2025-08-20 00:29:52 +0530

    Added forgot password feature

--- controllers/users.js
+++ controllers/users.js
@@ -1,50 +1,49 @@
-const Listing = require("../models/listing");
 const User = require("../models/user");
 
-module.exports.renderSignupForm =  (req, res) => {
-    res.render('users/signup.ejs');
-}
+// Signup form render
+module.exports.renderSignupForm = (req, res) => {
+    res.render("users/signup.ejs");
+};
 
-module.exports.signup = async (req, res) => {
-   try{
+// Signup logic
+module.exports.signup = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
 
-  let { username, email, password } = req.body;
-    let user = new User({ username, email });
-    const registeredUser  =await User.register(user, password);
-    console.log(registeredUser);
-    req.login(registeredUser, (err) => {
-        if (err) {
-            return next(err);
-        }
-            req.flash('success', 'Welcome to the app!');
-    res.redirect('/listings');
-    });
+        const user = new User({ username, email });
+        const registeredUser = await User.register(user, password);
 
-   } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/signup');
-    } 
-}
+        req.login(registeredUser, (err) => {
+            if (err) return next(err);
 
+            req.flash("success", "Welcome to the app!");
+            return res.redirect("/listings");
+        });
+
+    } catch (e) {
+        req.flash("error", e.message);
+        return res.redirect("/signup");
+    }
+};
+
+// Login form render
 module.exports.renderLoginForm = (req, res) => {
-    res.render('users/login.ejs');
+    res.render("users/login.ejs");
+};
 
-}
-
-module.exports.login = async (req, res) => {
-   
-    req.flash('success', `Welcome back`);
-    const redirectUrl = res.locals.redirectUrl || '/listings';
+// Login logic
+module.exports.login = (req, res) => {
+    req.flash("success", "Welcome back!");
+    const redirectUrl = res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
+};
 
-}
-
-module.exports.logout =  (req, res) => {
+// Logout logic
+module.exports.logout = (req, res, next) => {
     req.logout((err) => {
-        if (err) {
-         return   next(err);
-        }
-        req.flash('success', 'Goodbye!');
-        res.redirect('/listings');
+        if (err) return next(err);
+
+        req.flash("success", "Goodbye!");
+        res.redirect("/listings");
     });
-}
+};
--- routes/user.js
+++ routes/user.js
@@ -4,13 +4,17 @@ const User = require('../models/user.js');
 const wrapAsync = require('../utils/wrapAsync.js');
 const passport = require('passport');
 const { saveRedirectUrl } = require('../middleware.js');
-
 const usersController = require('../controllers/users.js');
 
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+
+// --- Signup ---
 router.route("/signup")
     .get(wrapAsync(usersController.renderSignupForm))
     .post(wrapAsync(usersController.signup));
 
+// --- Login ---
 router.route("/login")
     .get(wrapAsync(usersController.renderLoginForm))
     .post(
@@ -22,18 +26,101 @@ router.route("/login")
         usersController.login
     );
 
-router.get('/logout',usersController.logout);
+// --- Logout ---
+router.get('/logout', usersController.logout);
 
+// --- Forgot Password Form ---
 router.get('/forgot-password', (req, res) => {
     res.render('users/forgot-password');
 });
 
+// --- Forgot Password Submit ---
 router.post('/forgot-password', async (req, res) => {
-    // Yahan aap email bhejne ka logic ya flash message laga sakte hain
-    req.flash('success', 'If this email exists, a reset link will be sent.');
-    res.redirect('/login');
+    const { email } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            req.flash('success', 'If this email exists, a reset link will be sent.');
+            return res.redirect('/login');
+        }
+
+        // ✅ Generate token
+        const resetToken = crypto.randomBytes(20).toString('hex');
+
+        // Save token + expiry in DB
+        user.resetPasswordToken = resetToken;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+        await user.save();
+
+        // Reset link
+        const resetLink = `http://localhost:8080/reset-password/${resetToken}`;
+
+        // Send email
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.GMAIL_USER,
+                pass: process.env.GMAIL_PASS
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.GMAIL_USER,
+            to: user.email,
+            subject: 'Password Reset',
+            text: `Click here to reset your password: ${resetLink}`
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        req.flash('success', 'Check your email for a reset link.');
+        res.redirect('/login');
+    } catch (err) {
+        console.error(err);
+        req.flash('error', 'Something went wrong.');
+        res.redirect('/forgot-password');
+    }
 });
 
+// --- Reset Password Form ---
+router.get('/reset-password/:token', async (req, res) => {
+    const { token } = req.params;
+    const user = await User.findOne({
+        resetPasswordToken: token,
+        resetPasswordExpires: { $gt: Date.now() }
+    });
 
+    if (!user) {
+        req.flash('error', 'Token is invalid or expired.');
+        return res.redirect('/forgot-password');
+    }
+
+    res.render('users/reset-password', { token });
+});
+
+// --- Reset Password Submit ---
+router.post('/reset-password/:token', async (req, res) => {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const user = await User.findOne({
+        resetPasswordToken: token,
+        resetPasswordExpires: { $gt: Date.now() }
+    });
+
+    if (!user) {
+        req.flash('error', 'Token is invalid or expired.');
+        return res.redirect('/forgot-password');
+    }
+
+    await user.setPassword(password);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
+    await user.save();
+
+    req.flash('success', 'Password reset successful. You can now login.');
+    res.redirect('/login');
+});
 
 module.exports = router;
